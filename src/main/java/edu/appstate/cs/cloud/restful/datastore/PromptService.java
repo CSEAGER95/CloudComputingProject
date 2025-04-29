faf18598374757f2ade9445d2a63f50e8aa79646
CSEAGER95 <seagerjc@gmail.com>
2025-04-29 18:37:35 -0400

    created cloud run executable

--- src/main/java/edu/appstate/cs/cloud/restful/datastore/PromptService.java
+++ src/main/java/edu/appstate/cs/cloud/restful/datastore/PromptService.java
@@ -1,11 +1,9 @@
 package edu.appstate.cs.cloud.restful.datastore;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.cloud.datastore.Datastore;
@@ -14,101 +12,41 @@ import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.Key;
 import com.google.cloud.datastore.KeyFactory;
 import com.google.cloud.datastore.Query;
-import com.google.cloud.datastore.StringValue;
 
-import edu.appstate.cs.cloud.restful.models.Story;
-import edu.appstate.cs.cloud.restful.StoryGenerator;
+import edu.appstate.cs.cloud.restful.models.Prompt;
 
 @Service
 public class PromptService {
-
-    @Autowired
-    private StoryGenerator StoryGenerator;
-
-    private Datastore datastore = DatastoreOptions.newBuilder()
-    .setProjectId("teamprojectmccewenseager")
-    .build()
-    .getService();
-    private static final String ENTITY_KIND = "story";
+    private Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    private static final String ENTITY_KIND = "prompt";
     private final KeyFactory keyFactory = datastore.newKeyFactory().setKind(ENTITY_KIND);
 
-    public Story createStory(String prompt) throws IOException {
+    public Key createPrompt(Prompt prompt) {
         Key key = datastore.allocateId(keyFactory.newKey());
-
-        var generatedStoryText = StoryGenerator.generate(prompt);
-
-        Story story = new Story(new Story.Builder()
-            .withPrompt(prompt)
-            .withStory(generatedStoryText)
-            .withUpvotes(0)
-            .withDownvotes(0)
-            .withId(key.getName()));
-
-        Entity storyEntity = Entity.newBuilder(key)
-            .set(Story.Prompt, story.getPrompt())
-            .set(Story.Story, StringValue.newBuilder(story.getStory()).setExcludeFromIndexes(true).build())
-            .set(Story.Upvotes, story.getUpvotes())
-            .set(Story.Downvotes, story.getDownvotes())
-            .build();
-        datastore.put(storyEntity);
-        return story;
+        Entity promptEntity = Entity.newBuilder(key)
+                .set(Prompt.PROMPT, prompt.getPrompt())
+                .build();
+        datastore.put(promptEntity);
+        return key;
     }
 
-    public List<Story> getAllStories() {
+    public List<Prompt> getAllPrompts() {
         Query<Entity> query = Query.newEntityQueryBuilder()
                 .setKind(ENTITY_KIND)
                 .build();
         Iterator<Entity> entities = datastore.run(query);
-        return buildStories(entities);
+        return buildPrompts(entities);
     }
 
-    public Story upvoteStory(String storyId) {
-        Key key = keyFactory.newKey(storyId);
-        Entity entity = datastore.get(key);
-        
-        if (entity == null) {
-            throw new RuntimeException("Story not found with ID: " + storyId);
-        }
-        
-        long currentUpvotes = entity.getLong(Story.Upvotes);
-        entity = Entity.newBuilder(entity)
-                .set(Story.Upvotes, currentUpvotes + 1)
-                .build();
-        
-        datastore.update(entity);
-        return entityToStories(entity);
-    }
-    
-    public Story downvoteStory(String storyId) {
-        Key key = keyFactory.newKey(storyId);
-        Entity entity = datastore.get(key);
-        
-        if (entity == null) {
-            throw new RuntimeException("Story not found with ID: " + storyId);
-        }
-        
-        long currentDownvotes = entity.getLong(Story.Downvotes);
-        entity = Entity.newBuilder(entity)
-                .set(Story.Downvotes, currentDownvotes + 1)
-                .build();
-        
-        datastore.update(entity);
-        return entityToStories(entity);
+    private List<Prompt> buildPrompts(Iterator<Entity> entities) {
+        List<Prompt> prompts = new ArrayList<>();
+        entities.forEachRemaining(entity -> prompts.add(entityToPrompt(entity)));
+        return prompts;
     }
 
-    private List<Story> buildStories(Iterator<Entity> entities) {
-        List<Story> stories = new ArrayList<>();
-        entities.forEachRemaining(entity -> stories.add(entityToStories(entity)));
-        return stories;
-    }
-
-    private Story entityToStories(Entity entity) {
-        return new Story.Builder()
-                .withStory(entity.getString(Story.Story))
-                .withPrompt(entity.getString(Story.Prompt))
-                .withUpvotes(entity.getLong(Story.Upvotes))
-                .withDownvotes(entity.getLong(Story.Downvotes))
-                .withId(entity.getKey().getName())
+    private Prompt entityToPrompt(Entity entity) {
+        return new Prompt.Builder()
+                .withPrompt(entity.getString(Prompt.PROMPT))
                 .build();
     }
 }
